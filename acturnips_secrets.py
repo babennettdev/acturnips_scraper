c8a99206a1179d0fd807cdf49556985354ee2eed
babennettdev <babennettdev@gmail.com>
2020-04-11 14:05:26 -0400

    Adding acturnips_secrets.py

---
+++ acturnips_secrets.py
@@ -0,0 +1,15 @@
+#! /bin/env python
+"""
+Created on Sat Apr 11 13:10:55 2020
+
+@author: Blake Bennett
+"""
+
+class acturnips_secrets:
+    def __init__(self):
+        self.client_id = 'CLIENT_ID'
+        self.client_secret = 'CLIENT_SECRET'
+        self.user_agent = 'APP_NAME'
+        self.username = 'REDDIT_USERNAME'
+        self.password = 'REDDIT_PASSWORD'
+        
